86d62ee741bfbeadda5005e9deeb388769957bad
Shane Charles <shanecharles@burningicesolutions.com>
2016-01-16 09:39:49 -0600

    Day2 part 2

--- day2.fsx
+++ day2.fsx
@@ -10,6 +10,16 @@ let presentSlack (l, w, h) =
 
 let presentWrapping p = presentArea p + presentSlack p
 
+let ribbon (l, w, h) = [l;w;h] |> List.sort
+                       |> List.take 2
+                       |> List.sum 
+                       |> ((*) 2)
+
+let bow (l, w, h) = l * w * h
+
+let presentRibbon p = ribbon p + bow p
+
+
 let test = "1x2x3"
 let parseDimensions (p : string) = p.Split('x') |> Array.map Int32.Parse
                                    |> fun [|l; w; h|] -> (l, w, h)
@@ -18,3 +28,4 @@ let presents = IO.File.ReadAllLines(__SOURCE_DIRECTORY__ + @"\input2.txt")
                |> Array.map parseDimensions
 
 let totalWrapping = Array.map presentWrapping >> Array.sum
+let totalRibbon = Array.map presentRibbon >> Array.sum
