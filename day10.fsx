10c2017e9a984472e49e766b311027fcbc353fc7
Shane Charles <shanecharles@burningicesolutions.com>
2016-01-15 21:54:57 -0600

    Put day10 back.

--- day10.fsx
+++ day10.fsx
@@ -21,16 +21,4 @@ let lookSay (input : string) =
     | [] -> ""
     | h :: t -> lookSay' "" (1,h) t
 
-let lookSay' (input : string) = 
-    let rec lookSay'' acc (c, n) = function
-        | [] -> n.ToString() :: c.ToString() :: acc |> List.rev |> List.reduce (+)
-        | h :: t -> 
-            if h = n then lookSay'' acc (c+1,n) t
-            else
-                let acc' = (n.ToString()) :: c.ToString() :: acc
-                lookSay'' acc' (1,h) t
-    match input |> Seq.toList with 
-    | [] -> ""
-    | h :: t -> lookSay'' [] (1,h) t
-
-let run input t = {1 .. t} |> Seq.fold (fun i _ -> i |> lookSay') input
+let run input t = {1 .. t} |> Seq.fold (fun i _ -> i |> lookSay) input |> Seq.length
