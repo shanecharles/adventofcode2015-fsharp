dab5c296846096ce827b08020856ca7843be77a3
Shane Charles <shanecharles@burningicesolutions.com>
2016-01-17 10:56:41 -0600

    Day5 part 2

--- day5.fsx
+++ day5.fsx
@@ -7,6 +7,26 @@ let niceString input =
     let ns : string -> bool = Seq.windowed 2 >> Seq.exists (fun ca -> ns.Contains(ca))
     input |> mv && input |> cc && input |> ns |> not
 
+let niceString' input =
+    let removeTrips s = 
+        s |> Seq.fold 
+                    (fun (next : string) c -> 
+                                next.Replace(String.replicate 3 c, String.replicate 2 c)) input
+
+    let xyx = Seq.windowed 3 >> Seq.exists (fun [|a; _; c|] -> a = c)
+    let sanitized = input |> Seq.windowed 3
+                  |> Seq.choose (function
+                            | [| a; b; c |] when a = b && b = c -> Some (a.ToString())
+                            | a             -> None)
+                  |> Seq.distinct
+                  |> removeTrips
+    
+    let pairs = sanitized |> Seq.windowed 2 
+    let unique = pairs |> Seq.distinct
+    unique |> Seq.map (fun u -> pairs |> Seq.filter (fun p -> p = u) |> Seq.length)
+    |> Seq.exists (fun c -> c >= 2)
+    && input |> xyx
+    
 type Kid = 
     | Naughty
     | Nice
@@ -18,9 +38,19 @@ let (|IsNiceKid|_|) (input : string) =
 let NaughtyOrNice = function
     | IsNiceKid -> Nice
     | _         -> Naughty
-    
-let input = System.IO.File.ReadAllLines(__SOURCE_DIRECTORY__ + @"\input5.txt")
 
-let niceKids = input |> Array.map NaughtyOrNice
-               |> Array.filter (fun n -> n = Nice)
-               |> Array.length
+let (|IsNiceKid'|_|) (input : string) =
+    if input |> niceString' then Some ()
+    else None
+
+let NaughtyOrNice' = function
+    | IsNiceKid' -> Nice
+    | _         -> Naughty
+    
+let strings = System.IO.File.ReadAllLines(__SOURCE_DIRECTORY__ + @"\input5.txt")
+
+let NiceCount f = Array.map f >> Array.filter (fun n -> n = Nice)
+                  >> Array.length
+
+let niceKids = strings |> NiceCount NaughtyOrNice
+let niceKids' = strings |> NiceCount NaughtyOrNice'
