2147138c3b477d4788021a12db2d07d4af9bf7a1
Shane Charles <shanecharles@burningicesolutions.com>
2016-01-16 20:59:33 -0600

    Day3 part 1

---
+++ day3.fsx
@@ -0,0 +1,17 @@
+﻿let move (x,y) = function
+    | '^' -> (x, y + 1)
+    | 'v' -> (x, y - 1)
+    | '<' -> (x - 1, y)
+    | '>' -> (x + 1, y)
+    | _   -> (x, y)
+
+let countHouses dirs = 
+    dirs 
+    |> Seq.fold (fun (p,(s : (int * int) Set)) d -> 
+                    let p' = move p d
+                    (p', (s.Add p'))) ((0,0), Set.ofList [(0,0)])
+    |> fun (_, s) -> s |> Seq.length
+
+let input = System.IO.File.ReadAllText(__SOURCE_DIRECTORY__ + @"\input3.txt")
+
+input |> countHouses
