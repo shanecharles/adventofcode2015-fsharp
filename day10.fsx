a1db4c1773caad6f8014617fe38a4bda90f9ad57
Shane Charles <shanecharles@burningicesolutions.com>
2016-01-16 20:30:39 -0600

    theburningmonk

--- day10.fsx
+++ day10.fsx
@@ -9,6 +9,8 @@ let result1 = "312211"
 let start = "1"
 let one_iteration = "11"
 
+let input = "3113322113"
+
 let lookSay (input : string) = 
     let rec lookSay' acc (c, n) = function
         | [] -> sprintf "%s%d%c" acc c n
@@ -21,4 +23,13 @@ let lookSay (input : string) =
     | [] -> ""
     | h :: t -> lookSay' "" (1,h) t
 
-let run input t = {1 .. t} |> Seq.fold (fun i _ -> i |> lookSay) input |> Seq.length
+let lookSay' : string -> string = 
+    Seq.fold (fun acc x ->
+            match acc with
+            | (c,x') :: t when x' = x -> (c+1,x) :: t
+            | _ -> (1,x) :: acc) []
+    >> List.rev
+    >> Seq.collect (fun (c,x) -> sprintf "%d%c" c x)
+    >> function xs -> String.Join("",xs)
+
+let run f input t = {1 .. t} |> Seq.fold (fun i _ -> i |> f) input |> Seq.length
