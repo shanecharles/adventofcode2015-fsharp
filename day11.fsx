8889f3516ff087f1f05b60efcc947aa63828ead5
Shane Charles <shanecharles@burningicesolutions.com>
2016-01-24 11:19:26 -0600

    Day11 part 1

---
+++ day11.fsx
@@ -0,0 +1,28 @@
+﻿open System
+let eoc = byte 'z'
+
+let increment (input : string) = 
+    input |> Seq.toList
+    |> List.rev
+    |> List.fold (fun (acc, carry) c -> 
+                    match carry + (byte c) with 
+                    | x when x > eoc -> ('a' :: acc, 1uy)
+                    | x              -> ( (char x) :: acc, 0uy)
+                ) ([], 1uy)
+    |> fst |> fun cs -> String.Join("", cs)
+
+let hasConfusing : string -> bool = Seq.exists (fun c -> ['i'; 'o'; 'l'] |> List.exists (fun c' -> c' = c))
+let hasStraight : string -> bool = Seq.windowed 3 >> Seq.exists (function | ([| a; _; c |] as input) -> input = [| a .. c |] | _ -> false)
+let multipleUniquePairs : string -> bool = 
+    Seq.fold (fun (acc, c) i -> if i = c then c :: acc, i else acc, i) ([], ' ')
+    >> fst >> List.distinct >> List.length >> (<) 1
+
+let success input = input |> hasConfusing |> not 
+                    && input |> hasStraight
+                    && input |> multipleUniquePairs
+
+let input1 = "cqjxjnds"
+Seq.unfold (fun pwd -> let pwd' = increment pwd
+                       Some (pwd', pwd')) input1
+|> Seq.filter (fun pwd -> success pwd)
+|> Seq.head
