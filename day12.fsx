c49a0a306b6e86f11013550bdb57fa97fa4d6fda
Shane Charles <shanecharles@burningicesolutions.com>
2016-01-24 12:02:22 -0600

    Day12 part 1

---
+++ day12.fsx
@@ -0,0 +1,7 @@
+﻿open System
+let file = __SOURCE_DIRECTORY__ + @"\input12.txt"
+
+let sum = IO.File.ReadAllText(file).Split ([|'['; ']'; ','; '{';'}'; ':'|])
+          |> Seq.filter (fun s -> not (s.StartsWith("\"") || s.Trim() |> String.IsNullOrEmpty))
+          |> Seq.map Int32.Parse
+          |> Seq.sum
