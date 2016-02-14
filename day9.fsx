471245fa29b211addfed211eecb7aff6175b2591
Shane Charles <shanecharles@burningicesolutions.com>
2016-02-14 14:13:29 -0600

    Day 9 part 2

--- day9.fsx
+++ day9.fsx
@@ -12,26 +12,34 @@ let edges = file |> IO.File.ReadAllLines
             |> Array.filter (String.IsNullOrEmpty >> not)
             |> Array.map parseEdge
             |> Array.concat
-            |> Array.sortBy (fun {Weight=w} -> w)
             |> Array.toList
 
 let removeDest dest = List.filter (fun {To=t} -> t <> dest)
 
-let createPath es = 
+let shortest = List.sortBy (fun {Weight=w} -> w)
+let longest = List.sortByDescending (fun {Weight=w} -> w)
+
+let createPath fnsort es = 
     let rec path acc rs = 
         match rs with 
         | [] -> acc
         | _  -> 
             match acc with 
             | []     -> 
-                let dest = List.head rs
+                let dest = rs |> fnsort |> List.head
                 path (dest::acc) (rs |> removeDest dest.To |> removeDest dest.From) 
             | h :: _ ->
                 printfn "%s -> %s" h.From h.To
                 let dest = rs |> List.filter (fun {From=fr} -> fr = h.To)
-                           |> List.sortBy (fun {Weight=w} -> w)
+                           |> fnsort
                            |> List.head
                 path (dest :: acc) (rs |> removeDest dest.To) 
-    path [] es
+    es |> List.map (fun {From = f} -> f)
+    |> List.distinct
+    |> List.map (fun city -> es |> List.filter (fun {From=f} -> f = city) |> fnsort |> List.head)
+    |> List.map (fun dest -> path [dest] (es |> removeDest dest.To |> removeDest dest.From))
 
-let part1 = edges |> createPath |> List.map (fun {Weight = w} -> w) |> List.sum;;
+let calcWeight = List.map (fun {Weight=w} -> w) >> List.sum
+
+let part1 = edges |> createPath shortest |> List.map calcWeight |> List.min
+let part2 = edges |> createPath longest |> List.map calcWeight |> List.max
