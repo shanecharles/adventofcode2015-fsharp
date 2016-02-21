b642a129d91ebe6e5b6da8250814bb35deb4e232
Shane Charles <shanecharles@burningicesolutions.com>
2016-02-21 10:32:04 -0600

    day24 part 1

---
+++ day24.fsx
@@ -0,0 +1,47 @@
+open System
+
+let file = __SOURCE_DIRECTORY__ + "/input24.txt"
+let presents = file |> IO.File.ReadAllLines
+               |> Array.filter (String.IsNullOrEmpty >> not)
+               |> Array.map Int32.Parse
+               |> Array.rev
+
+let balance = presents |> Array.sum |> fun x -> x / 3
+let hi,lo = presents |> Array.partition (fun x -> x > 100)
+
+let combos = 
+  [ for i in 0 .. (hi.Length - 1) do 
+      yield hi |> Array.mapi (fun idx x -> if idx = i then -1 else x)
+            |> Array.filter (fun x -> x >= 0)
+            |> Array.toList ]
+
+let minLength bin = 
+  let rec makeBin acc a =
+    match acc |> Seq.sum with 
+    | x when x >= balance -> acc
+    | x -> match a with
+           | []       -> acc
+           | hd :: tl -> 
+             if x + hd <= balance then 
+               makeBin (hd :: acc) tl
+             else
+               makeBin acc tl
+  makeBin (bin |> List.rev) (lo |> Array.toList)
+
+let lows = combos |> List.map (fun l -> minLength l)
+let minPresents = lows |> List.map List.length |> List.min
+let minBins = lows |> List.filter (fun l -> l |> List.length = minPresents)
+              |> List.map List.rev
+              |> List.map (List.take 4)
+
+let makeCombosOf amount = 
+  seq { for i in { 0 .. (lo.Length - 2)} do 
+          for j in { i + 1 .. (lo.Length - 1)} do 
+            if lo.[i] + lo.[j] = amount then yield [lo.[i]; lo.[j]] }
+
+let minProduct = makeCombosOf >> Seq.map (Seq.reduce (*)) >> Seq.min
+
+let part1 = minBins |> List.map (fun l -> (l |> List.reduce (*), l |> List.sum))
+            |> List.map (fun (p,x) -> p, balance - x |> minProduct)
+            |> List.map (fun (p,x) -> int64 p * int64 x)
+            |> List.min
