b41b4944711efd529098372169ee81450080a06e
Shane Charles <shanecharles@burningicesolutions.com>
2016-02-21 13:58:17 -0600

    day25 part 1 solved

--- day25.fsx
+++ day25.fsx
@@ -1,3 +1,18 @@
-﻿let location = (2947, 3029)
-
+﻿let initial = 20151125L
+let location = (2947, 3029)
 let nextCode (pcode : int64) = pcode * 252533L % 33554393L
+
+let createGrid (x,y) =
+  seq { let mutable found = false
+        for i in 1 .. (2 * max x y) do 
+        for r in i .. -1 .. 1 do 
+          if not found then
+            let r,c = (r,i + 1 - r)
+            yield r,c
+            found <- r = x && c = y } 
+
+let calcCode init loc = 
+  loc |> createGrid |> Seq.skip 1
+  |> Seq.fold (fun code _ -> nextCode code) init
+
+let part1 = calcCode initial location
