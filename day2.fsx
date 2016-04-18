5cc7282139dfdd7af025334d46fc8b2e6e18734c
Shane Charles <shanecharles@burningicesolutions.com>
2016-04-18 17:41:19 -0500

    Refactor day 2

--- day2.fsx
+++ day2.fsx
@@ -1,26 +1,18 @@
 ﻿open System
 let area (x : int) y = x * y
 let double = ((*) 2) >> area
-
-let presentArea (l, w, h) = 
-    double l w + double w h + double h l
-
-let presentSlack (l, w, h) =
-    [|(area l w); (area w h); (area h l)|] |> Array.min
+let presentArea (l, w, h) = double l w + double w h + double h l
+let presentSlack (l, w, h) = 
+  [|(area l w); (area w h); (area h l)|] |> Array.min
 
 let presentWrapping p = presentArea p + presentSlack p
-
 let ribbon (l, w, h) = [l;w;h] |> List.sort
                        |> List.take 2
                        |> List.sum 
                        |> ((*) 2)
 
 let bow (l, w, h) = l * w * h
-
 let presentRibbon p = ribbon p + bow p
-
-
-let test = "1x2x3"
 let parseDimensions (p : string) = p.Split('x') |> Array.map Int32.Parse
                                    |> fun [|l; w; h|] -> (l, w, h)
 
