889ae370d7a4a3cc48aef3a419ee862bd4e571dd
shane charles <shanecharles@burningicesolutions.com>
2016-04-19 20:15:40 -0500

    Refactor day 3 part 2

--- day3.fsx
+++ day3.fsx
@@ -1,17 +1,28 @@
-﻿let move (x,y) = function
+﻿let input = System.IO.File.ReadAllText(__SOURCE_DIRECTORY__ + "/input3.txt")
+let move (x,y) = function
     | '^' -> (x, y + 1)
     | 'v' -> (x, y - 1)
     | '<' -> (x - 1, y)
     | '>' -> (x + 1, y)
     | _   -> (x, y)
+
 let part1 = Seq.scan move (0,0) >> Seq.distinct >> Seq.length
-let input = System.IO.File.ReadAllText(__SOURCE_DIRECTORY__ + "/input3.txt")
+
+// Part 3 - third refactor
+let part2 : string -> int = 
+  Seq.fold (fun ((h :: _) as xs ,ys) x -> (ys, move h x :: xs)) ([(0,0)],[(0,0)])
+  >> function (xs,ys) -> xs @ ys
+  >> Seq.distinct 
+  >> Seq.length
+
 input |> part1 |> printfn "Day 3 part 1: %d"
+input |> part2 |> printfn "Day 3 part 2: %d"
+
+// Part 2 - secondary refactor
 type Delivery =
   | Santa
   | Robo
 let whoDelivers x = if x % 2 = 0 then Santa else Robo
-
 input |> Seq.mapi (fun i x -> (whoDelivers i, x))
       |> Seq.fold 
         (fun (s,r) (w,x) -> match (w,s,r) with
@@ -22,18 +33,19 @@ input |> Seq.mapi (fun i x -> (whoDelivers i, x))
       |> Seq.distinct
       |> Seq.length
       |> printfn "Day 3 part 2: %d"
- 
-let countHouses dirs = 
-    dirs 
-    |> Seq.fold (fun (p,(s : (int * int) Set)) d -> 
-                    let p' = move p d
-                    (p', (s.Add p'))) ((0,0), Set.ofList [(0,0)])
-    |> fun (_, s) -> s |> Seq.length
 
-let countHouses' dirs = 
+// Part 2 - initial
+let part2' dirs = 
     dirs 
     |> Seq.fold (fun (p1, p2, (s : (int * int) Set)) d -> 
                     let p' = move p1 d
                     (p2, p', (s.Add p'))) ((0,0), (0,0), Set.ofList [(0,0)])
     |> fun (_, _, s) -> s |> Seq.length
 
+// Part 1 - initial
+let countHouses dirs = 
+    dirs 
+    |> Seq.fold (fun (p,(s : (int * int) Set)) d -> 
+                    let p' = move p d
+                    (p', (s.Add p'))) ((0,0), Set.ofList [(0,0)])
+    |> fun (_, s) -> s |> Seq.length
