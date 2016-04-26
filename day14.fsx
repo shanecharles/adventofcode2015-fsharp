835b780f3a2e76f0c2b8485315d9f4fcc58312b7
shane charles <shanecharles@burningicesolutions.com>
2016-04-26 11:13:28 -0500

    add missing day 14 part 2

--- day14.fsx
+++ day14.fsx
@@ -1,43 +1,34 @@
 ﻿open System
+type Reindeer = { Name : string; Speed : int; Duration : int; Rest : int }
+let parseLine (line : string) = let a = line.Split([|' '|])
+                                { Name = a.[0]
+                                  Speed = a.[3] |> Int32.Parse 
+                                  Duration = a.[6] |> Int32.Parse
+                                  Rest = a.[13] |> Int32.Parse }
 
-let data = """
-Rudolph can fly 22 km/s for 8 seconds, but then must rest for 165 seconds.
-Cupid can fly 8 km/s for 17 seconds, but then must rest for 114 seconds.
-Prancer can fly 18 km/s for 6 seconds, but then must rest for 103 seconds.
-Donner can fly 25 km/s for 6 seconds, but then must rest for 145 seconds.
-Dasher can fly 11 km/s for 12 seconds, but then must rest for 125 seconds.
-Comet can fly 21 km/s for 6 seconds, but then must rest for 121 seconds.
-Blitzen can fly 18 km/s for 3 seconds, but then must rest for 50 seconds.
-Vixen can fly 20 km/s for 4 seconds, but then must rest for 75 seconds.
-Dancer can fly 7 km/s for 20 seconds, but then must rest for 119 seconds.
-"""
-
-type Reindeer = { Name : string
-                  Speed : int 
-                  Duration : int
-                  Rest : int }
-
-let parseLine (line : string) = 
-    let a = line.Split([|' '|])
-    { Name = a.[0]
-      Speed = a.[3] |> Int32.Parse 
-      Duration = a.[6] |> Int32.Parse
-      Rest = a.[13] |> Int32.Parse }
-
-let deer = data.Split([|'\n'|])
-           |> Array.filter (String.IsNullOrEmpty >> not)
-           |> Array.map parseLine
+let deer = IO.File.ReadAllLines (__SOURCE_DIRECTORY__ + "/input14.txt")
+           |> Seq.filter (String.IsNullOrEmpty >> not)
+           |> Seq.map parseLine
 
 let raceTime = 2503
+let race time r = let fullRun = r.Duration * r.Speed
+                  let interval = r.Duration + r.Rest
+                  let cycle = time / interval
+                  let rem = time % interval
+                  r.Name, cycle * fullRun + 
+                          if rem >= r.Duration then fullRun
+                          else rem * r.Speed 
 
-let race time r = 
-    let fullRun = r.Duration * r.Speed
-    let interval = r.Duration + r.Rest
-    let cycle = time / interval
-    let rem = time % interval
-    r.Name, cycle * fullRun + 
-            if rem >= r.Duration then fullRun
-            else rem * r.Speed 
+let part1 : (Reindeer seq) -> int = Seq.map (race raceTime) >> Seq.maxBy snd >> snd
 
-let part1 = deer |> Array.map (race raceTime)
-            |> Array.sortByDescending snd
+let part2 xs = let racers = List.ofSeq xs
+               let dict = new System.Collections.Generic.Dictionary<string,int>()
+               racers |> List.iter (fun {Name=n} -> dict.Add(n, 0))
+               let awardPoint name = dict.[name] <- 1 + dict.[name]
+               let calcPoints x = racers |> Seq.map (race x) |> Seq.groupBy snd 
+                                  |> Seq.maxBy fst |> snd |> Seq.iter (fst >> awardPoint)
+               {1 .. raceTime} |> Seq.iter calcPoints
+               dict.Values |> Seq.max
+
+deer |> part1 |> printfn "Day 14 part 1 = %d"
+deer |> part2 |> printfn "Day 14 part 2 = %d"
